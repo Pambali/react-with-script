b6bfdf7cf1ac0fa65cefedefea2cc7fb69b8c2ab
Pambali <pambali.b@tringapps.com>
2021-10-07 12:16:14 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from "./components/List";
+import AddToList from './components/AddToList';
+   
+  export  interface IState{
+       people:{
+        name: string
+        age : number
+        url : string
+        note?: string
+  }[]
+}
+
+function App() {
+
+   const [people, setPeople] =useState<IState["people"]>([
+     {
+       name:"LeBron James",
+       url: "https://cdn.wionews.com/sites/default/files/styles/story_page/public/2019/10/15/113577-untitled-design-41.jpg",
+       age:36,
+      note:"Allegeric to staying on the same team",
+     }
+   ])  
+
+  return (
+    <div className="App">
+      <h1>People Invited to my Party</h1>
+      <List people = {people}/>
+      <AddToList people={people} setPeople={setPeople}/>
+    </div>
+  );
+}
+
+export default App;
